470495bbebf9e177ce9e2eebfad0842924937ef7
taoyuchuan <taoyuchuan@gmail.com>
2021-01-07 14:04:39 -0500

    Add payload in Hooks

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { buyCake, addCake } from '../redux'
 
 function HooksCakeContainer() {
+  const [number, setNumber] = useState(1)
   const numOfCakes = useSelector(state => state.cake.numOfCakes)
   const dispatch = useDispatch()
   return (
     <div>
       <h2>Number of cakes (hooks) - { numOfCakes }</h2>
-      <button onClick = { () => dispatch(buyCake()) }>Buy cake</button>
-      <button onClick = { () => dispatch(addCake()) }>Add Cake</button>
+      <input type="text" value = { number } onChange = { e => setNumber(Number(e.target.value)) } />
+      <button onClick = { () => dispatch(buyCake(number)) }>Buy { number } cake</button>
+      <button onClick = { () => dispatch(addCake(number)) }>Add { number } Cake</button>
     </div>
   )
 }
