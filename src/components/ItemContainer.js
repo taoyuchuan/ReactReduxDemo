aa1f0dfdaec9a12419e99a6b1c0229d0f4778db7
taoyuchuan <taoyuchuan@gmail.com>
2021-01-07 15:44:53 -0500

    Use ownProps to dispatch different actions

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream, addCake, addIceCream } from '../redux'
 
 function ItemContainer(props) {
   return(
     <div>
       <h2>Item - { props.item }</h2>
+      <button onClick = { props.buyItem } >Buy Item</button>
+      <button onClick = { props.addItem } >Add Item</button>
     </div>
   )
 }
@@ -19,4 +22,22 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchBuyFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream())
+
+  const dispatchAddFunction = ownProps.cake
+    ? () => dispatch(addCake())
+    : () => dispatch(addIceCream())
+
+  return {
+    buyItem: dispatchBuyFunction,
+    addItem: dispatchAddFunction
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ItemContainer)
